c15f6502256993b7a70dcc02203295bbb6f4850d
TEUBOOK\teu <taewoo0703@naver.com>
2023-09-26 22:52:46 +0900

    version

--- main.py
+++ main.py
@@ -29,7 +29,7 @@ import os
 import sys
 from devtools import debug
 
-VERSION = "POA : 0.1.1, Hatiko : 230923 23:00"
+VERSION = "POA : 0.1.1, Hatiko : 230926 23:00"
 app = FastAPI(default_response_class=ORJSONResponse)
 
 
